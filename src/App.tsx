a5a11942c71eb399f3d6249c7116db5a80023450
Shari Sun <shari09cxxcr@gmail.com>
2021-04-13 21:26:13 -0400

    Made template files
    Added a template for the react app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/** @jsx jsx */
+import React, {useEffect, useState} from 'react';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
+
+import {jsx, ThemeProvider} from 'theme-ui';
+
+export const App: React.FC = () => {
+  
+
+  return <h1>this is an app</h1>
+};
+
+export default App;
