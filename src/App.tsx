753ef44580168b5f4d7d59f0f56cb18700230f21
Shari Sun <shari09cxxcr@gmail.com>
2021-04-14 21:35:41 -0400

    Finished routing system

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
 /** @jsx jsx */
 import React, {useEffect, useState} from 'react';
-import {HashRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import {jsx, ThemeProvider} from 'theme-ui';
+import { Navigation } from './components/Navigation';
+import { AddPost } from './pages/AddPost';
+import { Home } from './pages/Home';
 
+import {theme} from './utils/theme';
+
+/**
+ * The main app.
+ * 
+ * This contains all the context providers and routing.
+ * 
+ * Theme provider: enables the app to use the theme specified in `./utils/theme.ts`.
+ * 
+ * 
+ * Routes:  
+ * `/` – the main page containing all the posts.  
+ * `/add-post` – the page to add new posts.  
+ * @returns The app
+ */
 export const App: React.FC = () => {
   
 
-  return <h1>this is an app</h1>
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route exact path = '/' component={Home} />
+          <Route path='/add-post' component={AddPost} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
+  );
 };
 
 export default App;
