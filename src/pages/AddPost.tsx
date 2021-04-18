06261cfc028b97a4d65dd1ca7affc6c9a72d7eb0
Emerald <44278515+EmeraldEntities@users.noreply.github.com>
2021-04-18 01:02:53 -0400

    Created basic add-post foundation
    Created the basic page and structure to add a page, minus the form elements.

--- src/pages/AddPost.tsx
+++ src/pages/AddPost.tsx
@@ -1,6 +1,9 @@
 /** @jsx jsx */
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import {jsx, SxStyleProp} from 'theme-ui';
+
+import { BoardContext, IBoardContext } from '../utils/context';
+import { HEADER_HEIGHT_PX } from '../components/Navigation'
 
 /**
  * This is the page that allows the user to add a new post.
@@ -14,14 +17,53 @@ import {jsx, SxStyleProp} from 'theme-ui';
  * @returns The add-post page. 
  */
 export const AddPost: React.FC = () => {
+  const board = useContext<IBoardContext>(BoardContext);
+
+  const [height, setHeight] = useState<number>(window.innerHeight)
+
+  /**
+   * Changes this add post page's height.
+   */
+  const adjustHeight = () => {
+    setHeight(window.innerHeight)
+  }
+
+  // TODO: figure out more efficient way of making sure the main body is the
+  // right height.
+  useEffect(() => {
+    window.addEventListener('resize', adjustHeight)
+
+    return () => {
+      window.removeEventListener('resize', adjustHeight)
+    }
+  }, [])
 
   const wrapperStyle: SxStyleProp = {
     variant: 'bodyWrapper',
+    height: height - HEADER_HEIGHT_PX,
+    overflow: 'hidden',
+    display: 'flex',
+    flexDirection: 'row',
   };
+
+  const photoStyle: SxStyleProp = {
+    width: '30%',
+    height: '100%',
+    backgroundImage: `linear-gradient(#EDEDED99, #EDEDED99), url(${board.bgImage})`,
+    backgroundSize: 'cover',
+  }
+
+  const formContainerStyle: SxStyleProp = {
+    width: '70%',
+    height: '100%',
+  }
 
   return (
     <div sx={wrapperStyle}>
-      This is the add post page.
+      <div sx={photoStyle} />
+      <div sx={formContainerStyle}>
+        // FORM COMPONENT GOES HERE
+      </div>
     </div>
   );
 };
